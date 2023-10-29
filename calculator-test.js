5c4271452eda8c8be42022585947176aef7bf2a4
KyamranR <kyamranr@gmail.com>
2023-10-29 15:02:01 -0400

    testing for 2 decimal

--- calculator-test.js
+++ calculator-test.js
@@ -7,7 +7,9 @@ it('should calculate the monthly rate correctly', function () {
 
 
 it("should return a result with 2 decimal places", function() {
-  
+  const values = {amount: 549324, years: 30, rate: 7.5}
+
+  expect(calculateMonthlyPayment(values)).toEqual('3840.95');
 });
 
 /// etc
