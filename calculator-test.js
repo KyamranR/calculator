d57444f0d179e9145715895b3078fa1ef1c196d9
KyamranR <kyamranr@gmail.com>
2023-10-29 14:55:53 -0400

    changed values to test more

--- calculator-test.js
+++ calculator-test.js
@@ -1,13 +1,13 @@
 
 it('should calculate the monthly rate correctly', function () {
-  const values ={amount: 10000, years: 8, rate: 5.8};
+  const values ={amount: 500000, years: 30, rate: 7.5};
 
-  expect(calculateMonthlyPayment(values)).toEqual('130.44');
+  expect(calculateMonthlyPayment(values)).toEqual('3496.07');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  
 });
 
 /// etc
