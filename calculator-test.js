b08649bd52cd4f1642f9b8c0bff15de8fca2d5b0
KyamranR <kyamranr@gmail.com>
2023-10-29 15:06:34 -0400

    testing 3 digit interest rate

--- calculator-test.js
+++ calculator-test.js
@@ -12,4 +12,8 @@ it("should return a result with 2 decimal places", function() {
   expect(calculateMonthlyPayment(values)).toEqual('3840.95');
 });
 
+it('should handle 3 digit interest rate', function(){
+  const values = {amount: 549324, years: 30, rate: 101}
+  expect(calculateMonthlyPayment(values)).toEqual('46234.77');
+})
 /// etc
